7f68739d2dd627eda0274f3f24ac969e33a03f09
Garrett Martin <garrett@blvrd.co>
2024-11-07 15:19:12 -0800

    Add colors for testing

--- fake_process.sh
+++ fake_process.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Define colors
+COLORS=(
+    "\033[0;31m"    # Red
+    "\033[0;32m"    # Green
+    "\033[0;33m"    # Yellow
+    "\033[0;34m"    # Blue
+    "\033[0;35m"    # Purple
+    "\033[0;36m"    # Cyan
+)
+RESET="\033[0m"     # Reset color
+
 # Array of random messages
 messages=(
     "System running smoothly"
@@ -16,13 +27,17 @@ messages=(
 
 # Infinite loop
 while true; do
-    # Get random message from array
-    random_index=$((RANDOM % ${#messages[@]}))
-    message="${messages[$random_index]}"
+    # Get random message and color
+    random_msg_index=$((RANDOM % ${#messages[@]}))
+    random_color_index=$((RANDOM % ${#COLORS[@]}))
 
-    # Print timestamp and message
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message"
+    message="${messages[$random_msg_index]}"
+    color="${COLORS[$random_color_index]}"
+
+    # Print timestamp and colored message
+    echo -e "${color}$(date '+%Y-%m-%d %H:%M:%S') - $message${RESET}"
 
     # Sleep for 1 second
-    sleep 0.1
+    sleep 0.5
 done
+
